ae557140df67c71eed6e8896e3c6850bb717866c
Luis Messias <luisfelipe_messias@hotmail.com>
2025-11-11 23:36:34 -0300

    Cleanup Main

--- src/main.cpp
+++ src/main.cpp
@@ -14,53 +14,22 @@ extern "C" {
     void app_main(void);
 }
 
-void engineMain1(void *args) {
-
-    EngineDriver engineDriver = {{25, 26}, 
-                                LEDC_TIMER_0, 
-                                std::pair{LEDC_CHANNEL_0, LEDC_CHANNEL_1}};
-
-    float p = 1;
-    engineDriver.setOutput(p); 
-
+void task(void *args) {
 	while(1){
-        p += 0.1;
-        if(p > 1){
-            p = -1;
-        }
-        // printf("%f \n", p);
-        // engineDriver.setOutput(p); 
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-	} 
-}
-
-void engineMain2(void *args) {
-
-    EngineDriver engineDriver = {{27, 14}, 
-                                LEDC_TIMER_1, 
-                                std::pair{LEDC_CHANNEL_2, LEDC_CHANNEL_3}};
-
-    float p = 0.3;
-    engineDriver.setOutput(p); 
-
-	while(1){
-        p -= 1;
-        if(p < -1){
-            p = 1;
-        }
-        // printf("%f \n", p);
-        // engineDriver.setOutput(p); 
         vTaskDelay(1000/portTICK_PERIOD_MS);
 	} 
 }
 
 void app_main()
 {	   
-    xTaskCreate(&engineMain1, "engineMain1", 2048, NULL, 5, NULL);
-    xTaskCreate(&engineMain2, "engineMain2", 2048, NULL, 5, NULL);
+    EngineDriver engineDriver = {{27, 14}, 
+                                LEDC_TIMER_0, 
+                                std::pair{LEDC_CHANNEL_0, LEDC_CHANNEL_1}};
+    engineDriver.setOutput(1); 
 
     EncoderDriver encoderDriver(21, 22, 823.1);
-
+    
+    xTaskCreate(&task, "task", 2048, NULL, 5, NULL);
     while(1){
         printf("%f\n", encoderDriver.getRPM());
         vTaskDelay(1000/portTICK_PERIOD_MS);
