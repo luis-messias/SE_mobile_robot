b0580f002e83953c545dcc3b505a9c5a64acdf56
Luis Messias <luisfelipe_messias@hotmail.com>
2025-11-12 00:06:33 -0300

    PDI fist implementation

--- src/main.cpp
+++ src/main.cpp
@@ -22,16 +22,35 @@ void task(void *args) {
 
 void app_main()
 {	   
-    EngineDriver engineDriver = {{27, 14}, 
+    EngineDriver engineDriver = {{14, 27}, 
                                 LEDC_TIMER_0, 
                                 std::pair{LEDC_CHANNEL_0, LEDC_CHANNEL_1}};
-    engineDriver.setOutput(1); 
+    engineDriver.setOutput(0); 
 
     EncoderDriver encoderDriver(21, 22, 823.1);
-    
     xTaskCreate(&task, "task", 2048, NULL, 5, NULL);
+
+    float setPoint = 60;
+    float rpm;
+    float erro = 0;
+    float sumErro = 0;
+    float lastErro = 0;
+    float k = 0.003;
+    float kd = 0.003;
+    float ki = 0.003;
+    float out;
+
+    vTaskDelay(3000/portTICK_PERIOD_MS);
     while(1){
-        printf("%f\n", encoderDriver.getRPM());
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+        rpm = encoderDriver.getRPM();
+        erro = setPoint - rpm;
+        sumErro += erro;
+
+        out = k * erro + ki * sumErro + kd * (erro - lastErro);
+        printf("%f %f %f %f %f\n", rpm, erro, sumErro, erro - lastErro, out);
+        engineDriver.setOutput(out); 
+
+        lastErro = erro;
+        vTaskDelay(100/portTICK_PERIOD_MS);
     }
 }
